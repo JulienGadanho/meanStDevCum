4bd4a84f28a3497d84acfbb4beefc3c48258fe7f
JulienGadanho <jgadanho.ads@gmail.com>
2020-06-23 03:34:54 +0200

    Update test.php

--- test.php
+++ test.php
@@ -3,23 +3,27 @@
 
 if(isset($_POST["submit"])) {
   
+    //create imagick object
     $image = new Imagick($_FILES['fileToUpload']['tmp_name']);
 
+    //get datas of 3 channels
     $blue = $image->getImageChannelMean(Imagick::COLOR_ALPHA);
     $red = $image->getImageChannelMean(Imagick::COLOR_RED);
     $green = $image->getImageChannelMean(Imagick::COLOR_GREEN);
 
+    //calculate mean cumulative value
     $meancum = $blue['mean'] / 255;
     $meancum += $red['mean'] / 255;
     $meancum += $green['mean'] / 255;
     $meancum = round($meancum / 3, 0);
 
-
+    //calculate standard deviation cumulative value
     $stdcum = $blue['standardDeviation'] / 255;
     $stdcum += $red['standardDeviation'] / 255;
     $stdcum += $green['standardDeviation'] / 255;
     $stdcum = round($stdcum / 3, 0);
-
+    
+    //display
     echo '<b>Mean Cum :</b> ' .$meancum.'<br>';
     echo '<b>StdDev Cum :</b> '.$stdcum.'<br><br>';
 }
